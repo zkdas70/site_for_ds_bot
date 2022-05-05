082edbdd2ed889e81bb156e58e15cd7dc79689c6
zkda <kostya.zinich.su>
2022-05-05 18:27:17 +0700

    зата от слива

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,8 @@
+with open('token.png', encoding='utf-8') as f_in:
+    token = f_in.readlines()[0]
+
 settings = {
-    #'token': ,
+    'token': token,
     #'token': ',
     'bot': 'Valera',
     'id': 849713822250958868,
